6e75466a510f29f6c0560cd6ea5376da45aa17ef
DJAngel973 <angel.djuancho@gmail.com>
2025-06-28 22:20:02 -0500

    feat(player):add Player class with attributes, constructor, and encapsulated methods

---
+++ src/main/java/co/edu/poli/model/Player.java
@@ -0,0 +1,38 @@
+package co.edu.poli.model;
+
+//Create Player class to enter a user into the game
+public class Player {
+
+    //Attributes for the player
+    private String name;
+    private int points;
+    private int lives;
+
+    //Constructor for the initial state of the player object
+    public Player(String name){
+        this.name = name;
+        this.points = 0;
+        this.lives = 3;
+    }
+
+    //Getters y setters
+    public String getName(){return this.name;}
+    public void setName(String name){this.name = name;}
+
+    public int getPoints(){return this.points;}
+    public void setPoints(int points){this.points = points;}
+
+    public int getLives(){return this.lives;}
+    public void setLives(int lives){this.lives = lives;}
+
+    //Methods to update game status
+    public void addPoints(int points){this.points += points;}
+    public void subtractLives(int lives){--this.lives;}
+
+    //Method for sum lives 'increase lives if possible'(Incrementar vidas si es posible.)
+    public void increaseLivesPossible(){
+        if (this.lives < 3){
+            ++this.lives;
+        }
+    }
+}
